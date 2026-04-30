1e4e8b9a0b5a910d7816fb6089566dcc1be03847
David Nidever <dnidever@montana.edu>
2026-04-29 20:07:03 -0600

    adding distances.py

---
+++ python/hgetargeting/distances.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+
+def distance(tab):
+    """ Estimates distances from the photometry with dereddened J-Ks colors and Hmag
+        and assuming solar metallicity isochrone """
+
+    pass
