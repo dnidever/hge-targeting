9f6b2f89c7ff8057ccff0c4501b6ca1668095428
David Nidever <dnidever@montana.edu>
2026-04-29 19:27:44 -0600

    renamed import package name to hgetargeting

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='hge-targeting',
+setup(name='hgetargeting',
       version='1.0.0',
       description='AS5 HGE targeting',
       author='David Nidever and others',
       author_email='dnidever@montana.edu',
       url='https://github.com/sdss/hge-targeting/',
-      packages=['hge-targeting'],
+      packages=['hgetargeting'],
       package_dir={'':'python'},
       requires=['numpy','astropy','scipy']
 )
